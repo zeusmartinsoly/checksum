ee664895feb2d4e569592efcf0aaf7de80abda49
Karim T <44747814+zeusmartinsoly@users.noreply.github.com>
2025-05-31 04:21:55 +0300

    Update checksum.js

--- api/checksum.js
+++ api/checksum.js
@@ -1,16 +1,20 @@
+import { createHash } from 'crypto';
+
 export default function handler(req, res) {
-  const crypto = require('crypto');
+  try {
+    const { AppSecret, Nonce, CurTime } = req.body;
 
-  const { AppSecret, Nonce, CurTime } = req.body;
+    if (!AppSecret || !Nonce || !CurTime) {
+      return res.status(400).json({ error: 'Missing fields' });
+    }
 
-  if (!AppSecret || !Nonce || !CurTime) {
-    return res.status(400).json({ error: 'Missing fields' });
+    const hash = createHash('sha256')
+      .update(AppSecret + Nonce + CurTime)
+      .digest('hex');
+
+    return res.status(200).json({ CheckSum: hash });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
-
-  const hash = crypto
-    .createHash('sha256')
-    .update(AppSecret + Nonce + CurTime)
-    .digest('hex');
-
-  return res.status(200).json({ CheckSum: hash });
 }
